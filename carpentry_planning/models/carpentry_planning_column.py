2addd10dbc64ab5740ca7e433cc9f4b24173cd9c
Arnaud LAYEC <arnaudlayec@gmail.com>
2024-11-27 18:39:58 +0100

    cancel previous commit

--- carpentry_planning/models/carpentry_planning_column.py
+++ carpentry_planning/models/carpentry_planning_column.py
@@ -44,8 +44,7 @@ class CarpentryPlanningColumn(models.Model):
         inverse='_inverse_identifier_ref',
         help='Identifier value is needed when 2 columns get their source from same Model.'
              ' The identifier model must have a `column_id` field holding the routing logic.'
-             ' Example: Task Needs to route towards `Needs (Method)` or `Needs (Field)`',
-        precompute=True # for constrain `_constrain_identifier_required()`
+             ' Example: Task Needs to route towards `Needs (Method)` or `Needs (Field)`'
     )
     identifier_res_id = fields.Many2oneReference(
         model_field='identifier_res_model',
