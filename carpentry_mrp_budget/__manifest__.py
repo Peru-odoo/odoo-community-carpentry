474bfb054906c0a6bdb9c3f5d63548d4cf6fd085
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-01-16 19:43:53 +0100

    carpentry_purchase: date widget in form instead of datetime

---
+++ carpentry_mrp_budget/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "MRP Budget",
+    'summary': "Consume positions budget from workcenter times",
+    'author': 'Arnaud LAYEC',
+    'website': 'https://github.com/arnaudlayec/odoo-community-carpentry',
+    'license': 'AGPL-3',
+
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'category': 'Carpentry/Carpentry',
+    'version': '16.0.1.0.1',
+
+    'depends': [
+        'mrp_account', # Odoo
+
+        'mrp_workcenter_cost_history', # other
+
+        # carpentry
+        'carpentry_mrp',
+        'carpentry_position_budget',
+    ],
+    'data': [
+        # data
+        'views/mrp_workcenter.xml',
+    ]
+}
---
+++ carpentry_mrp_budget/models/mrp_production.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, exceptions, _, Command
+
+class ManufacturingOrder(models.Model):
+    _inherit = ['mrp.production']
+
+    pass
