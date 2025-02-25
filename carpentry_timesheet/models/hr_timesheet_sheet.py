a83a5f0dd0e652c9618cee49b77b3458cf9c96e9
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-02-25 16:58:50 +0100

    [fix] timesheet/leave : don't timesheet on task configured on leaves

--- carpentry_timesheet/models/hr_timesheet_sheet.py
+++ carpentry_timesheet/models/hr_timesheet_sheet.py
@@ -62,10 +62,10 @@ class HrTimesheetSheet(models.Model):
             ]
 
         # 4.
-        if hasattr(self.available_task_ids, 'is_timeoff_task'):
+        if hasattr(self.env['project.task'], 'is_timeoff_task'):
             domain = AND([domain, [('is_timeoff_task', '=', False)]])
         print('domain', domain)
-        
+
         self.available_task_ids = self.available_task_ids.filtered_domain(domain)
         
         return res
