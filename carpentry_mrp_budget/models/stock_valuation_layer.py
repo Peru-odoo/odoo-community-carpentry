16f7374eaf5a13f71ee022de804cac90371d75b3
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-03-06 19:15:20 +0100

    project on stock.valuation.layer

--- carpentry_mrp_budget/models/stock_valuation_layer.py
+++ carpentry_mrp_budget/models/stock_valuation_layer.py
@@ -6,5 +6,7 @@ class StockValuationLayer(models.Model):
     _inherit = ['stock.valuation.layer']
 
     project_id = fields.Many2one(
-        related='stock_move_id.project_id'
+        related='stock_move_id.project_id',
+        store=True,
+        index=True,
     )
