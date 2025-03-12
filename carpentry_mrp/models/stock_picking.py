e254c2dd98d44700fe5be900a8a871726cce08f8
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-03-13 00:28:54 +0100

    debug picking auto reservation

--- carpentry_mrp/models/stock_picking.py
+++ carpentry_mrp/models/stock_picking.py
@@ -19,12 +19,12 @@ class StockPicking(models.Model):
         store=True,
         readonly=False
     )
-    mrp_production_ids = fields.One2many(
-        # related from procurement group
-        # when modified, we link the picking to the MO's procurement group
-        inverse='_inverse_mrp_production_ids',
-        domain="[('launch_ids', 'in', launch_ids)]"
-    )
+    # mrp_production_ids = fields.One2many(
+    #     # related from procurement group
+    #     # when modified, we link the picking to the MO's procurement group
+    #     inverse='_inverse_mrp_production_ids',
+    #     domain="[('launch_ids', 'in', launch_ids)]"
+    # )
 
     #===== Compute =====#
     @api.depends('mrp_production_ids', 'purchase_id')
@@ -33,9 +33,9 @@ class StockPicking(models.Model):
             po = picking.purchase_id
             mo = picking.mrp_production_ids
 
-            picking.launch_ids = po.launch_ids | mo.launch_ids
-            if po or len(mo) == 1:
-                picking.description = po.description if po else mo.description
+            # picking.launch_ids = po.launch_ids | mo.launch_ids
+            # if po or len(mo) == 1:
+            #     picking.description = po.description if po else mo.description
 
     @api.model
     def _search_launch_ids(self, operator, value):
@@ -45,8 +45,8 @@ class StockPicking(models.Model):
         ]
 
     # !!!!!!!! TO BE TESTED !!!!!!!!
-    def _inverse_mrp_production_ids(self):
-        for picking in self:
-            mo = picking.production_ids
-            if len(mo) == 1:
-                picking.group_id = mo.procurement_group_id
+    # def _inverse_mrp_production_ids(self):
+    #     for picking in self:
+    #         mo = picking.production_ids
+    #         if len(mo) == 1:
+    #             picking.group_id = mo.procurement_group_id
