e2a89cfe85b8c036319a561acefc7c3d4da75a05
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-03-13 00:47:59 +0100

    debug picking auto reservation

--- carpentry_mrp_budget/models/stock_move.py
+++ carpentry_mrp_budget/models/stock_move.py
@@ -29,7 +29,7 @@ class StockMove(models.Model):
 
             print('new_distrib', new_distrib)
             print('new_distrib.keys()', new_distrib and new_distrib.keys())
-            print('[Command.set([int(x) for x in new_distrib.keys()])]', [Command.set([int(x) for x in new_distrib.keys()])])
+            print('[Command.set([int(x) for x in new_distrib.keys()])]', new_distrib and [Command.set([int(x) for x in new_distrib.keys()])])
 
             # synthetic: only analytic_ids (no % distribution)
             move.analytic_ids = new_distrib and [Command.set([int(x) for x in new_distrib.keys()])]
