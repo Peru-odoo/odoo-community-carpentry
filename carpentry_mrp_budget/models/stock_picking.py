f25be0a87cbfe9be12ea51e8e41fffc11cacbf44
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-03-13 00:31:48 +0100

    debug picking auto reservation

--- carpentry_mrp_budget/models/stock_picking.py
+++ carpentry_mrp_budget/models/stock_picking.py
@@ -32,8 +32,8 @@ class StockPicking(models.Model):
     def _compute_budget_analytic_ids(self):
         """ Picking budgets are from moves' analytic distribution """
         for picking in self:
-            project_budgets = picking.project_id.budget_line_ids.analytic_account_id
-            picking.budget_analytic_ids = picking.move_ids.analytic_ids.filtered('is_project_budget') & project_budgets
+            project_budgets = picking.project_id._origin.budget_line_ids.analytic_account_id
+            picking.budget_analytic_ids = picking.move_ids.analytic_ids._origin.filtered('is_project_budget') & project_budgets
     
     def _get_total_by_analytic(self):
         """ Group-sum price of move
