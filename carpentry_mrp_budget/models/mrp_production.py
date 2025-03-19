5cbca5e0317629e7207fab17cd1b9dc16777ddd1
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-03-19 17:02:47 +0100

    production: time budgets

--- carpentry_mrp_budget/models/mrp_production.py
+++ carpentry_mrp_budget/models/mrp_production.py
@@ -11,6 +11,8 @@ class ManufacturingOrder(models.Model):
     #====== Fields ======#
     affectation_ids = fields.One2many(domain=[('section_res_model', '=', _name)])
     affectation_ids_production = fields.One2many(
+        comodel_name='carpentry.group.affectation',
+        inverse_name='section_id',
         domain=[('section_res_model', '=', _name), ('budget_type', '=', 'production')]
     )
     # affectation_ids_production = fields.One2many(
