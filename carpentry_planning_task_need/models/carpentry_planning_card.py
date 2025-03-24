87e3ebdfe8233c0671eac1458e5c1799ce346ce6
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-03-24 15:37:21 +0100

    carpentry needs v2 : simplified by tasks

---
+++ carpentry_planning_task_need/models/carpentry_planning_card.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+from odoo.osv import expression
+
+class CarpentryPlanningCard(models.Model):
+    """ Add required field for specific Tasks cards """
+    _inherit = ["carpentry.planning.card"]
+
+    project_task_id = fields.Many2one(
+        # for research by launches
+        comodel_name='project.task',
+        string='Task'
+    )
+
+    week_deadline = fields.Integer(compute='_compute_fields')
+    
+    def _get_fields(self):
+        return super()._get_fields() + ['week_deadline']
