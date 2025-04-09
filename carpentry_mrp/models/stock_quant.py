85475a49ac55ea2558a4bbd6c6ad11378e01bf11
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-04-09 15:00:44 +0200

    debug

--- carpentry_mrp/models/stock_quant.py
+++ carpentry_mrp/models/stock_quant.py
@@ -45,55 +45,55 @@ class StockQuant(models.Model):
                 quant.is_outdated = True
 
 
-    @api.constrains("product_id", "quantity", "stock_move_ids")
-    def check_negative_qty(self):
-        """ This part is copied from the module `stock_no_negative`,
-            from the OCA project `stock-logistics-workflow`,
-            adapted for the field `quantity_without_outgoing_raw_material` 
-        """
-        print('=== check_negative_qty (start) ===')
+    # @api.constrains("product_id", "quantity", "stock_move_ids")
+    # def check_negative_qty(self):
+    #     """ This part is copied from the module `stock_no_negative`,
+    #         from the OCA project `stock-logistics-workflow`,
+    #         adapted for the field `quantity_without_outgoing_raw_material` 
+    #     """
+    #     print('=== check_negative_qty (start) ===')
 
-        # To provide an option to skip the check when necessary.
-        # e.g. mrp_subcontracting_skip_no_negative - passes the context
-        # for subcontracting receipts.
-        if self.env.context.get("skip_negative_qty_check"):
-            return
-        p = self.env["decimal.precision"].precision_get("Product Unit of Measure")
-        check_negative_qty = (
-            config["test_enable"] and self.env.context.get("test_stock_no_negative")
-        ) or not config["test_enable"]
-        if not check_negative_qty:
-            return
+    #     # To provide an option to skip the check when necessary.
+    #     # e.g. mrp_subcontracting_skip_no_negative - passes the context
+    #     # for subcontracting receipts.
+    #     if self.env.context.get("skip_negative_qty_check"):
+    #         return
+    #     p = self.env["decimal.precision"].precision_get("Product Unit of Measure")
+    #     check_negative_qty = (
+    #         config["test_enable"] and self.env.context.get("test_stock_no_negative")
+    #     ) or not config["test_enable"]
+    #     if not check_negative_qty:
+    #         return
 
-        for quant in self:
-            disallowed_by_product = (
-                not quant.product_id.allow_negative_stock
-                and not quant.product_id.categ_id.allow_negative_stock
-            )
-            disallowed_by_location = not quant.location_id.allow_negative_stock
-            if (
-                float_compare(quant.quantity_without_outgoing_raw_material, 0, precision_digits=p) == -1
-                and quant.product_id.type == "product"
-                and quant.location_id.usage in ["internal", "transit"]
-                and disallowed_by_product
-                and disallowed_by_location
-            ):
-                msg_add = ""
-                if quant.lot_id:
-                    msg_add = _(" lot {}").format(quant.lot_id.name_get()[0][1])
-                raise ValidationError(
-                    _(
-                        "You cannot validate this stock operation because the "
-                        "stock level of the product '{name}'{name_lot} would "
-                        "become negative "
-                        "({q_quantity}) on the stock location '{complete_name}' "
-                        "and negative stock is "
-                        "not allowed for this product and/or location."
-                    ).format(
-                        name=quant.product_id.display_name,
-                        name_lot=msg_add,
-                        q_quantity=quant.quantity,
-                        complete_name=quant.location_id.complete_name,
-                    )
-                )
-        print('=== (end) check_negative_qty ===')
+    #     for quant in self:
+    #         disallowed_by_product = (
+    #             not quant.product_id.allow_negative_stock
+    #             and not quant.product_id.categ_id.allow_negative_stock
+    #         )
+    #         disallowed_by_location = not quant.location_id.allow_negative_stock
+    #         if (
+    #             float_compare(quant.quantity_without_outgoing_raw_material, 0, precision_digits=p) == -1
+    #             and quant.product_id.type == "product"
+    #             and quant.location_id.usage in ["internal", "transit"]
+    #             and disallowed_by_product
+    #             and disallowed_by_location
+    #         ):
+    #             msg_add = ""
+    #             if quant.lot_id:
+    #                 msg_add = _(" lot {}").format(quant.lot_id.name_get()[0][1])
+    #             raise ValidationError(
+    #                 _(
+    #                     "You cannot validate this stock operation because the "
+    #                     "stock level of the product '{name}'{name_lot} would "
+    #                     "become negative "
+    #                     "({q_quantity}) on the stock location '{complete_name}' "
+    #                     "and negative stock is "
+    #                     "not allowed for this product and/or location."
+    #                 ).format(
+    #                     name=quant.product_id.display_name,
+    #                     name_lot=msg_add,
+    #                     q_quantity=quant.quantity,
+    #                     complete_name=quant.location_id.complete_name,
+    #                 )
+    #             )
+    #     print('=== (end) check_negative_qty ===')
