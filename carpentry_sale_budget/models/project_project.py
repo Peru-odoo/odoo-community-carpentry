7d1074e3025e102d78ee62395949213ed91b3065
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-07-06 23:16:55 +0200

    [IMP] actual margins

--- carpentry_sale_budget/models/project_project.py
+++ carpentry_sale_budget/models/project_project.py
@@ -6,16 +6,14 @@ from collections import defaultdict
 class Project(models.Model):
     _inherit = ['project.project']
 
-    # fees
+    #---- fees ----
     fees_prorata = fields.Monetary(
         string='Prorata',
         compute='_compute_budget_fees_and_margins',
-        currency_field='currency_id'
     )
     fees_structure = fields.Monetary(
         string='Structure',
         compute='_compute_budget_fees_and_margins',
-        currency_field='currency_id'
     )
     # fees' rates
     fees_prorata_rate = fields.Float(
@@ -26,18 +24,15 @@ class Project(models.Model):
         string='Structure (%)',
         default=0.0,
     )
-
-    # margins
+    #---- initial margins: [Market - Budget] ----
     margin_contributive = fields.Monetary(
         string='Contributive margin',
         compute='_compute_budget_fees_and_margins',
-        currency_field='currency_id',
         help='Total market - All budgets - Only prorata fees (i.e. not structure fees)'
     )
     margin_costs = fields.Monetary(
         string='Margin on costs',
         compute='_compute_budget_fees_and_margins',
-        currency_field='currency_id',
         help='Total market - All budgets - All fees',
     )
     # margins' rates
@@ -49,28 +44,84 @@ class Project(models.Model):
         string='Margin on costs (%)',
         compute='_compute_budget_fees_and_margins'
     )
+    #---- actual margins: [Reserved Budget - Real Expense] ----
+    margin_contributive_actual = fields.Monetary(
+        string='Contributive margin (actual)',
+        compute='_compute_budget_fees_and_margins',
+        help='Reserved Budget - Real Expense - Only prorata fees (i.e. not structure fees)'
+    )
+    margin_costs_actual = fields.Monetary(
+        string='Margin on costs (actual)',
+        compute='_compute_budget_fees_and_margins',
+        help='Reserved Budget - Real Expense - All fees',
+    )
+    # actuel margins' rates
+    margin_contributive_actual_rate = fields.Integer(
+        string='Contributive margin (actual) (%)',
+        compute='_compute_budget_fees_and_margins'
+    )
+    margin_costs_actual_rate = fields.Integer(
+        string='Margin on costs (actual) (%)',
+        compute='_compute_budget_fees_and_margins',
+    )
+    budget_reservation_progress = fields.Integer(
+        string='Budget reservation progress (%)',
+        compute='_compute_budget_fees_and_margins',
+    )
 
     # sale order line updated status (for warning)
     budget_up_to_date = fields.Boolean(
         compute='_compute_budget_up_to_date',
+        help='Reliability score of actuals indicator, computed as [Reserved budget / Available budget]',
     )
 
     #===== Compute: fees, margins =====#
     @api.depends('fees_prorata_rate', 'fees_structure_rate', 'market_reviewed', 'budget_line_sum')
     def _compute_budget_fees_and_margins(self):
-        for project in self:
-            project._compute_budget_fees_and_margins_one()
+        # 2. Reserved budget & real expense
+        rg_expense = self.env['carpentry.budget.expense']._read_group(
+            domain=[('project_id', 'in', self.ids)],
+            fields=['quantity_affected:sum', 'amount_expense:sum'],
+            groupby=['project_id'],
+        )
+        mapped_expense = {
+            x['project_id'][0]: {'reserved': x['quantity_affected'], 'expense': x['amount_expense']}
+            for x in rg_expense
+        }
         
-    def _compute_budget_fees_and_margins_one(self):
+        for project in self:
+            project._compute_budget_fees_and_margins_one(mapped_expense)
+        
+    def _compute_budget_fees_and_margins_one(self, mapped_expense):
         # fees
         self.fees_prorata   = self.fees_prorata_rate * self.market_reviewed / 100
         self.fees_structure = self.fees_structure_rate * self.market_reviewed / 100
-        # margins
+
+        # initial margins
         self.margin_costs        = self.market_reviewed - self.budget_line_sum - self.fees_prorata - self.fees_structure
         self.margin_contributive = self.market_reviewed - self.budget_line_sum - self.fees_prorata # ie. on direct costs only
+        # actual margins
+        x = mapped_expense.get(self._origin.id, {'reserved': 0.0, 'expense': 0.0})
+        self.margin_costs_actual           = x['reserved'] - x['expense'] - self.fees_prorata - self.fees_structure
+        self.margin_contributive_actual    = x['reserved'] - x['expense'] - self.fees_prorata
+
         # rates
-        self.margin_costs_rate        = bool(self.market_reviewed) and self.margin_costs / self.market_reviewed * 100
-        self.margin_contributive_rate = bool(self.market_reviewed) and self.margin_contributive / self.market_reviewed * 100
+        if self.market_reviewed:
+            # initial margins
+            self.margin_costs_rate        = self.margin_costs / self.market_reviewed * 100
+            self.margin_contributive_rate = self.margin_contributive / self.market_reviewed * 100
+            # actual margins
+            self.margin_costs_actual_rate        = self.margin_costs_actual / self.market_reviewed * 100
+            self.margin_contributive_actual_rate = self.margin_contributive_actual / self.market_reviewed * 100
+        else:
+            # initial margins
+            self.margin_costs_rate        = 0.0
+            self.margin_contributive_rate = 0.0
+            # actual margins
+            self.margin_costs_actual_rate        = 0.0
+            self.margin_contributive_actual_rate = 0.0
+        
+        self.budget_reservation_progress = bool(self.budget_line_sum) and x['reserved'] / self.budget_line_sum * 100
 
     #===== Compute : sale order line budget updated status =====#
     @api.depends('sale_order_ids.order_line', 'sale_order_ids.order_line.budget_updated', 'sale_order_ids.state')
